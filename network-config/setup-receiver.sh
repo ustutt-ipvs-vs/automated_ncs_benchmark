3ed4422b9fb0dbdf898581be7d07d6746f4c1f52
David Augustat <code@davidaugustat.com>
2022-11-29 18:40:39 +0100

    Scripts and config files for network setup

---
+++ network-config/setup-receiver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run this command on the computer that should be used as the receiver.
+
+# Rename existing netplan configs:
+for file in /etc/netplan/*yaml
+do
+    sudo mv "$file" "$file.bak"
+done
+
+# Copy netplan config to config directory:
+sudo cp 01-receiver-netplan-config.yaml /etc/netplan/.
+
+# Enable netplan config:
+sudo netplan generate
+sudo netplan apply
