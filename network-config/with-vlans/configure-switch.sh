56207bef87b5cbb5b6b665ab002c9facc5b8cadb
David Augustat <code@davidaugustat.com>
2022-12-30 01:13:54 +0100

    Add ingress-qos-map and egress-qos-map to network config
    This is to create a mapping from VLAN PCP to skb->priority and vice versa

--- network-config/with-vlans/configure-switch.sh
+++ network-config/with-vlans/configure-switch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Setup interfaces for VLANs (on VLAN ID 100)
-sudo ip link add link enp1s0f0 name enp1s0f0.100 type vlan id 100
-sudo ip link add link enp1s0f1 name enp1s0f1.100 type vlan id 100
+# Setup interfaces for VLANs (on VLAN ID 100):
+# The ingress-qos-map maps the VLAN PCP to the skb->priority.
+# The egress-qos-map maps the skb->priority to the VLAN PCP.
+sudo ip link add link enp1s0f0 name enp1s0f0.100 type vlan id 100 ingress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7 egress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7
+sudo ip link add link enp1s0f1 name enp1s0f1.100 type vlan id 100 ingress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7 egress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7
 
 # Create bridge
 sudo ip link add name br0 type bridge
@@ -23,3 +25,6 @@ sudo ip link set dev br0 up
 # Set IP address of switch pc:
 sudo ip address add 10.0.1.4/24 dev br0
 
+
+# The custom scheduling then needs to be applied to enp1s0f1.100.
+# The priority provided through the VLAN PCP can be read from the skb->priority.
