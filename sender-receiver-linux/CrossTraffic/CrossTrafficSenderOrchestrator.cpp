ac2118cb1dcf08779b324b90eaa804d0183e403a
David Augustat <code@davidaugustat.com>
2023-01-29 17:44:44 +0100

    CrossTrafficSenderOrchestrator: Change default initialization

--- sender-receiver-linux/CrossTraffic/CrossTrafficSenderOrchestrator.cpp
+++ sender-receiver-linux/CrossTraffic/CrossTrafficSenderOrchestrator.cpp
@@ -1,11 +1,17 @@
 #include "CrossTrafficSenderOrchestrator.h"
+#include "../Scheduling/ConstantPriority.h"
 
 void CrossTrafficSenderOrchestrator::initializeSendersByDefault() {
     std::string host = "10.0.1.2";
 
-    senders.emplace_back(std::make_shared<CrossTrafficSender>(2'000'000, 1'000'000, 5, 125'000, host, 3000, "Sender1"));
-    senders.emplace_back(std::make_shared<CrossTrafficSender>(2'000'000, 1'000'000, 5, 125'000, host, 3001, "Sender2"));
-    senders.emplace_back(std::make_shared<CrossTrafficSender>(2'000'000, 1'000'000, 5, 125'000, host, 3002, "Sender3"));
+    senders.emplace_back(std::make_shared<CrossTrafficSender>(new ConstantPriority(0), 160'000, host, 3001, "Sender 0"));
+    senders.emplace_back(std::make_shared<CrossTrafficSender>(new ConstantPriority(1), 160'000, host, 3001, "Sender 1"));
+    senders.emplace_back(std::make_shared<CrossTrafficSender>(new ConstantPriority(2), 160'000, host, 3001, "Sender 2"));
+    senders.emplace_back(std::make_shared<CrossTrafficSender>(new ConstantPriority(3), 160'000, host, 3001, "Sender 3"));
+    senders.emplace_back(std::make_shared<CrossTrafficSender>(new ConstantPriority(4), 160'000, host, 3001, "Sender 4"));
+    senders.emplace_back(std::make_shared<CrossTrafficSender>(new ConstantPriority(5), 160'000, host, 3001, "Sender 5"));
+    senders.emplace_back(std::make_shared<CrossTrafficSender>(new ConstantPriority(6), 160'000, host, 3001, "Sender 6"));
+    senders.emplace_back(std::make_shared<CrossTrafficSender>(new ConstantPriority(7), 160'000, host, 3001, "Sender 7"));
 }
 
 void CrossTrafficSenderOrchestrator::initializeSendersWithParameters() {
