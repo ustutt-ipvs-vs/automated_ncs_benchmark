22a4ede85ae276987a766c69e9a516eff689a5ac
David Augustat <code@davidaugustat.com>
2023-01-29 17:35:47 +0100

    Use MPTB in PendulumSenderMain

--- sender-receiver-linux/PendulumSenderSide/PendulumSenderMain.cpp
+++ sender-receiver-linux/PendulumSenderSide/PendulumSenderMain.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "PendulumSender.h"
+#include "../Scheduling/ConstantPriority.h"
+#include "../Scheduling/MultiPriorityTokenBucket.h"
 
 std::string device = "/dev/ttyACM0";
 
@@ -23,9 +25,26 @@ void sigIntHandler(int signal){
     exit(0);
 }
 
+std::vector<double> samplingPeriodsToDataRates(std::vector<double> samplingPeriods){
+    std::vector<double> dataRates;
+    int frameSizeOfSample = 71;
+    for(double samplingPeriod : samplingPeriods){
+        double dataRate = (1000.0 / samplingPeriod) * frameSizeOfSample;
+        dataRates.emplace_back(dataRate);
+    }
+    return dataRates;
+}
+
 int main(){
     signal(SIGINT, sigIntHandler);
-    PriorityDeterminer* determiner = new TokenBucketPrioTest(b, r, initialPriority);
+    //PriorityDeterminer* determiner = new TokenBucketPrioTest(b, r, initialPriority);
+    //PriorityDeterminer* determiner = new ConstantPriority(1);
+    //PriorityDeterminer* determiner = new TokenBucketPrioTest(7100, 710, 0);
+
+    std::vector<double> dataRates = samplingPeriodsToDataRates({100, 84, 67, 50, 40, 30, 20, 10});
+    PriorityDeterminer* determiner = new MultiPriorityTokenBucket(7100, 710, 8, 0, dataRates);
+
+
     sender = new PendulumSender(determiner, device, host, port);
     sender->start();
 }
