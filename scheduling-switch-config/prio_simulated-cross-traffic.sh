5f278fc5648e919d63cc727f97e152dffe9ed088
David Augustat <code@davidaugustat.com>
2023-02-02 13:34:18 +0100

    Switch Scheduling Config: Select predefined parameters via command line arguments

--- scheduling-switch-config/prio_simulated-cross-traffic.sh
+++ scheduling-switch-config/prio_simulated-cross-traffic.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
+# Usage:
+# Option 1: Use predefined values for low, medium, or high latency:
+# ./prio_simulated-cross-traffic.sh [ls|lm|lh]
+#
+# Option 2: Use custom values for the 8 priority classes:
+# ./prio_simulated-cross-traffic.sh [delay0] [delay1] [delay2] [delay3] [delay4] [delay5] [delay6] [delay7]
+
 interface=enp1s0f0
 netemQueueLength=20 # in packets
-delays=(0 5 10 15 20 25 30 35)  # delays for priority classes 0 to 7 in milliseconds
+
+# Values are in milliseconds:
+low_latency_values=(2 4 6 8 10 12 14 16)
+medium_latency_values=(5 10 15 20 25 30 35 40)
+high_latency_values=(10 20 30 40 50 60 70 80)
+
+# If first argument is 'ls' use low latency values, if it is 'lm' use medium latency values, 
+# if it is 'lh' use high latency values, otherwise use the given values:
+if [ "$1" == "ls" ]; then
+    delays=("${low_latency_values[@]}")
+elif [ "$1" == "lm" ]; then
+    delays=("${medium_latency_values[@]}")
+elif [ "$1" == "lh" ]; then
+    delays=("${high_latency_values[@]}")
+else
+    delays=("$@")
+    # Abort if not exactly 8 values are given:
+    if [ ${#delays[@]} -ne 8 ]; then
+        echo "Error: Exactly 8 values must be given as arguments."
+        exit 1
+    fi
+fi
+
+# Print delay values:
+echo "Delays for priority classes 0 to 7 in milliseconds:"
+echo ${delays[@]}
+
 
 # Reduce queue lengths:
 bash "$(dirname "$0")/setup-queue-length.sh" $interface
