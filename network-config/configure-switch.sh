623b5fd5f53fcd378195ca96a61ea345a5b2b2f6
David Augustat <code@davidaugustat.com>
2023-03-19 13:03:28 +0100

    Update network config scripts

--- network-config/configure-switch.sh
+++ network-config/configure-switch.sh
@@ -5,21 +5,26 @@
 # The egress-qos-map maps the skb->priority to the VLAN PCP.
 sudo ip link add link enp1s0f0 name enp1s0f0.100 type vlan id 100 ingress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7 egress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7
 sudo ip link add link enp1s0f1 name enp1s0f1.100 type vlan id 100 ingress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7 egress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7
+sudo ip link add link enp1s0f3 name enp1s0f3.100 type vlan id 100 ingress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7 egress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7
 
 # Create bridge
 sudo ip link add name br0 type bridge
 sudo ip link set dev enp1s0f0.100 master br0
 sudo ip link set dev enp1s0f1.100 master br0
+sudo ip link set dev enp1s0f3.100 master br0
 
 # Enable promiscious mode
 sudo ip link set dev enp1s0f0.100 promisc on
 sudo ip link set dev enp1s0f1.100 promisc on
+sudo ip link set dev enp1s0f3.100 promisc on
 
 # Turn on all involved interfaces:
 sudo ip link set dev enp1s0f0.100 up
 sudo ip link set dev enp1s0f1.100 up
+sudo ip link set dev enp1s0f3.100 up
 sudo ip link set dev enp1s0f0 up
 sudo ip link set dev enp1s0f1 up
+sudo ip link set dev enp1s0f3 up
 sudo ip link set dev br0 up
 
 # Set IP address of switch pc:
