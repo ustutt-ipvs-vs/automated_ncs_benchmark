b731e1717bc434901fd115780aff962ccb8bf10f
David Augustat <code@davidaugustat.com>
2023-10-02 18:15:21 +0200

    Create SenderConfig class

---
+++ sender-receiver-linux/PendulumSenderSide/SenderConfig.cpp
@@ -0,0 +1,143 @@
+#include <fstream>
+#include "SenderConfig.h"
+#include "../nlohmann/json.hpp"
+
+double SenderConfig::getB() const {
+    return b;
+}
+
+double SenderConfig::getR() const {
+    return r;
+}
+
+int SenderConfig::getNumPriorities() const {
+    return numPriorities;
+}
+
+const std::vector<double> &SenderConfig::getThresholds() const {
+    return thresholds;
+}
+
+const std::vector<int> &SenderConfig::getPrioMapping() const {
+    return prioMapping;
+}
+
+const std::vector<double> &SenderConfig::getCosts() const {
+    return costs;
+}
+
+SenderConfig::SenderConfig(std::string filename) {
+    std::ifstream configFile(filename);
+    if (!configFile.good()) {
+        throw std::runtime_error("Config file does not exist");
+    }
+
+    nlohmann::json configJson = nlohmann::json::parse(configFile);
+
+    b = configJson["b"];
+    r = configJson["r"];
+    numPriorities = configJson["numPriorities"];
+
+    thresholds = configJson["thresholds"].get<std::vector<double>>();
+    prioMapping = configJson["prioMapping"].get<std::vector<int>>();
+    costs = configJson["costs"].get<std::vector<double>>();
+
+    if (configJson.contains("historySize")) {
+        historySize = configJson["historySize"];
+    } else {
+        historySize = 100; // default value
+    }
+    if (configJson.contains("samplingPeriods")) {
+        samplingPeriods = configJson["samplingPeriods"].get<std::vector<int>>();
+    } else {
+        samplingPeriods = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10}; // default value
+    }
+    if (configJson.contains("initialPriorityClass")) {
+        initialPriorityClass = configJson["initialPriorityClass"];
+    } else {
+        initialPriorityClass = 0; // default value
+    }
+    if(configJson.contains("serialDeviceName")) {
+        serialDeviceName = configJson["serialDeviceName"];
+    } else {
+        serialDeviceName = "auto";
+    }
+    automaticallyFindSerialDevice = (serialDeviceName == "auto");
+    receiverAddress = configJson["receiverAddress"];
+
+    configFile.close();
+
+    if(numPriorities < 1) {
+        throw std::runtime_error("numPriorities must be at least 1");
+    }
+
+    if(historySize < 0){
+        throw std::runtime_error("historySize must be at least 0");
+    }
+
+    if (thresholds.size() != numPriorities - 1) {
+        throw std::runtime_error("Number of thresholds is not numPriorities - 1");
+    }
+    if (prioMapping.size() != numPriorities) {
+        throw std::runtime_error("Number of priorities does not match numPriorities");
+    }
+    if (costs.size() != numPriorities) {
+        throw std::runtime_error("Number of costs does not match numPriorities");
+    }
+}
+
+int SenderConfig::getHistorySize() const {
+    return historySize;
+}
+
+const std::vector<int> &SenderConfig::getSamplingPeriods() const {
+    return samplingPeriods;
+}
+
+const std::string &SenderConfig::getSerialDeviceName() const {
+    return serialDeviceName;
+}
+
+bool SenderConfig::isAutomaticallyFindSerialDevice() const {
+    return automaticallyFindSerialDevice;
+}
+
+const std::string &SenderConfig::getReceiverAddress() const {
+    return receiverAddress;
+}
+
+int SenderConfig::getInitialPriorityClass() const {
+    return initialPriorityClass;
+}
+
+std::string SenderConfig::toString() const {
+    std::string result = "SenderConfig:\n";
+    result += "b: " + std::to_string(b) + "\n";
+    result += "r: " + std::to_string(r) + "\n";
+    result += "numPriorities: " + std::to_string(numPriorities) + "\n";
+    result += "thresholds: ";
+    for(double threshold : thresholds){
+        result += std::to_string(threshold) + " ";
+    }
+    result += "\n";
+    result += "prioMapping: ";
+    for(int prio : prioMapping){
+        result += std::to_string(prio) + " ";
+    }
+    result += "\n";
+    result += "costs: ";
+    for(double cost : costs){
+        result += std::to_string(cost) + " ";
+    }
+    result += "\n";
+    result += "historySize: " + std::to_string(historySize) + "\n";
+    result += "samplingPeriods: ";
+    for(int period : samplingPeriods){
+        result += std::to_string(period) + " ";
+    }
+    result += "\n";
+    result += "serialDeviceName: " + serialDeviceName + "\n";
+    result += "automaticallyFindSerialDevice: " + std::to_string(automaticallyFindSerialDevice) + "\n";
+    result += "receiverAddress: " + receiverAddress + "\n";
+    return result;
+}
---
+++ sender-receiver-linux/PendulumSenderSide/SenderConfig.h
@@ -0,0 +1,92 @@
+/**
+ * This is a class representing a JSON config file for the sender.
+ * The configuration options in this file are targeted at MPTB scheduling.
+ *
+ * For JSON parsing, the nlohmann/json library is used.
+ *
+ * An example config file:
+ *  {
+ *   "b": 200.0,
+ *   "r": 30.0,
+ *   "numPriorities": 4,
+ *   "thresholds": [0, -200, -400],
+ *   "prioMapping": [0, 1, 2, 7],
+ *   "costs": [1, 1, 1, 0],
+ *   "historySize": 100,
+ *   "samplingPeriods": [100, 90, 80, 70, 60, 50, 40, 30, 20, 10],
+ *   "initialPriorityClass": 0,
+ *   "serialDeviceName": "/dev/ttyACM0",
+ *   "receiverAddress": "10.0.1.3"
+ * }
+ *
+ *  The default values are, if not specified otherwise:
+ *  historySize = 100
+ *  samplingPeriods = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+ *  initialPriorityClass = 0
+ *  serialDeviceName = "auto"   // automatically find the device by scanning /dev/ttyACM*
+ * */
+
+#ifndef SENDER_RECEIVER_LINUX_SENDERCONFIG_H
+#define SENDER_RECEIVER_LINUX_SENDERCONFIG_H
+
+
+#include <vector>
+#include <string>
+
+class SenderConfig {
+private:
+
+    // MPTB parameters:
+    double b;
+    double r;
+    int numPriorities;
+    int initialPriorityClass;
+    std::vector<double> thresholds;
+    std::vector<int> prioMapping;
+    std::vector<double> costs;
+
+    // Teensy parameters:
+    int historySize;
+public:
+    const std::vector<int> &getSamplingPeriods() const;
+
+private:
+    std::vector<int> samplingPeriods;
+    std::string serialDeviceName;
+    bool automaticallyFindSerialDevice;
+
+    // Network parameters:
+    std::string receiverAddress;
+
+
+public:
+    int getHistorySize() const;
+
+    const std::string &getSerialDeviceName() const;
+
+    bool isAutomaticallyFindSerialDevice() const;
+
+    const std::string &getReceiverAddress() const;
+
+    SenderConfig(std::string filename);
+
+    double getB() const;
+
+    double getR() const;
+
+    int getNumPriorities() const;
+
+    const std::vector<double> &getThresholds() const;
+
+    const std::vector<int> &getPrioMapping() const;
+
+    const std::vector<double> &getCosts() const;
+
+    int getInitialPriorityClass() const;
+
+    std::string toString() const;
+
+};
+
+
+#endif //SENDER_RECEIVER_LINUX_SENDERCONFIG_H
